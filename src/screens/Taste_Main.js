f02fddd078339c85ebe5bd0f822e0aa9b7a5ec4b
existentialist2001 <jusongjun@naver.com>
2022-05-31 16:20:13 +0900

    조건부 styling 적용 전

--- src/screens/Taste_Main.js
+++ src/screens/Taste_Main.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 import NicknameModal from "../components/NicknameModal";
 
+import styled from "styled-components";
+
 //모달 임포트
 import MainButton from "../components/NicknameModal";
 import RelationModal from "../components/RelationModal";
@@ -38,30 +40,24 @@ const Taste_Main = (props) => {
   };
 
   return (
-    <div style={styles.screen}>
-      <div style={styles.gradientContainer}>
-        <div style={styles.header}>
-          <button
-            style={styles.headerButton}
-            onClick={() => console.log("클릭")}
-          >
+    <ScreenContainer>
+      <GradientContainer>
+        <HeaderContainer>
+          <BackButton onClick={() => console.log("clicked")}>
             <MdArrowBackIosNew color="white" size="35" />
-          </button>
-          <button style={styles.headerButton}>
+          </BackButton>
+
+          <ThreeDotButton>
             <HiDotsHorizontal color="white" size="35" />
-          </button>
-        </div>
-        <div style={styles.main}>
-          <div style={styles.mainTextsContainer}>
-            <div style={styles.mainFirst} class="mainFirst">
-              <div style={styles.nameText}>이지은</div>
+          </ThreeDotButton>
+        </HeaderContainer>
+
+        <MainContainer>
+          <MainSectionContainer>
+            <MainNamesContainer>
+              <NameText>이지은</NameText>
               <div>
-                <button
-                  style={styles.nicknameButton}
-                  onClick={() => setNicknameModal(true)}
-                >
-                  {nickname}
-                </button>
+                <NickNameButton onClick={()=>setNicknameModal(true)}>{nickname}</NickNameButton>
               </div>
 
               <NicknameModal
@@ -69,41 +65,37 @@ const Taste_Main = (props) => {
                 onCancel={cancelChangeNickname}
                 onChange={setNickname}
               />
-            </div>
-            <div className="mainSecond">
-              <div>
-                <button
-                  style={styles.relationButton}
-                  onClick={() => setRelationModal(true)}
-                >
-                  {relationStatus}
-                </button>
-              </div>
+            </MainNamesContainer>
+            <MainRelationContainer>
+              <RelationButton onClick={() => setRelationModal(true)}>
+              {relationStatus}
+              </RelationButton>
+
               <RelationModal
                 visible={relationModal}
                 onCancel={cancelChangeRelation}
                 onChange={setRelationStatus}
               />
-            </div>
-          </div>
-        </div>
+            </MainRelationContainer>
+          </MainSectionContainer>
+        </MainContainer>
 
-        <div style={styles.middleBar}>
-          <div style={styles.middleBarTextContainer}>
-            <Link style={styles.middleBarText} to="/basicinfo">
+        <MiddleBarContainer>
+          <MiddleBarLinkContainer>
+            <Link className="MiddleBarLinkTexts" to="/basicinfo">
               기본정보
             </Link>
-            <Link style={styles.middleBarText} to="/taste">
+            <Link className="MiddleBarLinkTexts" to="/taste">
               취향
             </Link>
-            <Link style={styles.middleBarText} to="/history">
+            <Link className="MiddleBarLinkTexts" to="/history">
               히스토리
             </Link>
-          </div>
-        </div>
-      </div>
+          </MiddleBarLinkContainer>
+        </MiddleBarContainer>
+      </GradientContainer>
 
-      <div style={styles.information}>
+      <InformationContainer>
         <Route path="/basicinfo">
           <Taste_basicinfo_screen />
         </Route>
@@ -116,70 +108,192 @@ const Taste_Main = (props) => {
         <Route path="/history">
           <Taste_history_screen />
         </Route>
-      </div>
-    </div>
+      </InformationContainer>
+    </ScreenContainer>
   );
 };
 
 export default Taste_Main;
 
+const ScreenContainer = styled.div`
+  //flex 컨테이너로 만들어주고, main axis를 세로 축으로
+  display: flex;
+  flex-direction: column;
+  //screen이 전체 flex 컨테이너이기 때문에, viewport를 기준으로 100% 설정
+  width: 100vw;
+  height: 100vh;
+`;
+
+const GradientContainer = styled.div`
+  height: 40%;
+  width: 100%;
+  background: linear-gradient(to left top, #6a5acd 45%, #00bfff);
+`;
+
+const HeaderContainer = styled.div`
+  //헤더는 전체의 10%만큼, 그라데이션의 25%만큼 차지
+  height: 25%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MainContainer = styled.div`
+  //main은 전체의 20%만큼, 그라데이션의 50%만큼 차치
+  height: 50%;
+  width: 100%;
+
+  //main을 flexbox로 만들어 주어야, main안의 요소들이(이름 호칭 관계 묶음, 행성 이미지) 가로로 배열됨
+  display: flex;
+  justify-content: space-between;
+`;
+
+//mainTextsContainer와 planetContainer은 부모인 main의 width를 35%, 65%씩, 100% height
+const MainSectionContainer = styled.div`
+  width: 50%;
+  height: 100%;
+
+  //
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const MainNamesContainer = styled.div`
+  display: flex;
+  justify-content:space-evenly;
+
+  /*height를 주어서 container box의 height 크기를 설정하지 않으면, container box의 height가 자식요소의 크기에의해 상대적으로 결정되므로 
+  레이아웃과 크기 설정에 지장 */
+  height: 50%;
+
+  align-items: center;
+
+  /*box-sizing을 border-box로 하면, width 100%를 border, padding, content가 나눠 갖는다, 따라서 padding값을 어떤 값으로 적용하더라도
+  전체 box 크기는 변하지 않는다, 고정된 전체 box 크기 안에서 padding과 content 크기가 조정된다 */
+  box-sizing:border-box;
+  width: 100%;
+  padding-left:6%;
+`;
+const MainRelationContainer =styled.div`
+width:100%;
+height:50%;
+display:flex;
+justify-content: center;
+
+`
+
+const MiddleBarContainer = styled.div`
+  //middleBar는 전체의 10%만큼,  그라데이션의 25%차지
+  height: 25%;
+  width: 100%;
+
+  //
+  background-color: white;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+
+  //
+  display: flex;
+  justify-content: center;
+`;
+
+const BackButton = styled.button`
+  background-color: transparent;
+  border-color: transparent;
+`;
+
+const ThreeDotButton = styled.button`
+  background-color: transparent;
+  border-color: transparent;
+`;
+
+const NameText = styled.div`
+  color: white;
+  font-weight: bold;
+  font-size: 27px;
+`;
+
+const NickNameButton = styled.button`
+  //todo 아래 속성값들 반응형으로 바꾸어야 함.
+  border-radius: 98px;
+  width: 53px;
+  height: 33px;
+  font-size: 14px;
+  color: white;
+  font-weight: bold;
+
+  //borderColor 속성만 주면, 뒷 배경색과 색이 합쳐져서 렌더링 된다.
+  border: solid 1px;
+  border-color: white;
+
+  //버튼 색을 투명하게 해서 뒷 배경색으로 보이게.
+  background-color: transparent;
+`;
+
+const RelationButton = styled.button`
+  
+  //opacity로 주면 하위 요소들까지 적용되어서, rgba로 색과 투명도를 동시에 주었다.
+  background-color:rgba(255,255,255, 0.3);
+  color:#fff;
+  text-align:center;
+  font-weight:bold;
+  font-family: Roboto;
+  
+  //TODO 반응형으로
+  font-size:14px;
+  width: 117px;
+  height: 42px;
+  border-radius: 20px;
+  
+  //button이 기본값으로 border가 있기 때문에 없애준다,,
+  border-width:0;
+  
+
+
+`;
+
+
+const InformationContainer = styled.div`
+  //information은 전체의 60%만큼 차지
+  height: 60%;
+  width: 100%;
+
+  //! 왜 안먹히니..
+  border-top-width: 10px;
+  border-top-color: black;
+`;
+
+const MiddleBarLinkContainer = styled.div`
+  //middleBar(뷰포트 가로 전체)의 80%
+  width: 80%;
+  //middleBar의 높이 100%(높이도 설정해주지 않으면, content 크기만큼만 가져감)
+  height: 100%;
+  //
+  display: flex;
+  //justify-content: space-between;
+  align-items: center;
+
+  .MiddleBarLinkTexts {
+    text-decoration: none;
+    color: #627cec;
+    font-weight: bold;
+    font-size: 1.4em;
+
+    //! 복기필요 
+    box-sizing:border-box;
+    width:33%;
+    //height:100%;
+    text-align: center;
+  }
+  .MiddleBarLinkTexts:active{
+    box-sizing:border-box;
+    border-bottom:1px solid black;
+  }
+
+`;
 const styles = {
-  screen: {
-    //flex 컨테이너로 만들어주고, main axis를 세로 축으로
-    display: "flex",
-    flexDirection: "column",
-    //screen이 전체 flex 컨테이너이기 때문에, viewport를 기준으로 100% 설정
-    width: "100vw",
-    height: "100vh",
-  },
-  gradientContainer: {
-    height: "40%",
-    width: "100%",
-    background: "linear-gradient(to left top, #6a5acd 45%, #00bfff)",
-  },
-  header: {
-    //헤더는 전체의 10%만큼, 그라데이션의 25%만큼 차지
-    height: "25%",
-
-    //
-    width: "82%",
-    paddingLeft: "9%",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  headerButton: {
-    backgroundColor: "transparent",
-    borderColor: "transparent",
-  },
-
-  main: {
-    //main은 전체의 20%만큼, 그라데이션의 50%만큼 차치
-    height: "50%",
-    width: "100%",
-
-    //main을 flexbox로 만들어 주어야, main안의 요소들이(이름 호칭 관계 묶음, 행성 이미지) 가로로 배열됨
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  mainFirst: {
-    display: "flex",
-
-    //todo 이거 아래 속성들 적용이 안됨..왜지??
-    //width: "28%",
-    //justifyContent: "space-between",
-  },
-
-  //mainTextsContainer와 planetContainer은 부모인 main의 width를 35%, 65%씩, 100% height
-  mainTextsContainer: {
-    width: "35%",
-    height: "100%",
-
-    //
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   planetContainer: {
     width: "65%",
     height: "100%",
@@ -190,94 +304,6 @@ const styles = {
     height: "130%",
   },
   //<div style={styles.planetContainer}><img style={styles.planetImg} src={require("../images/planet.png")}/></div>
-  nameText: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: "2.1875em",
-  },
-  nicknameButton: {
-    //todo 아래 속성값들 반응형으로 바꾸어야 함.
-    borderRadius: 98,
-    width: 72,
-    height: 47,
-    fontSize: 17,
-
-    color: "white",
-    fontWeight: "bold",
-
-    //borderColor 속성만 주면, 뒷 배경색과 색이 합쳐져서 렌더링 된다.
-    border: "solid 1px",
-    borderColor: "white",
-
-    //버튼 색을 투명하게 해서 뒷 배경색으로 보이게.
-    backgroundColor: "transparent",
-  },
-  relationButton: {
-    opacity: 0.25,
-    borderRadius: "20px",
-    border: "solid 1px #fff",
-    backgroundColor: "#fff",
-
-    //TODO 반응형으로
-    width: 117,
-    height: 47,
-
-    //todo 수정필요
-    fontSize: "14px",
-    fontWeight: 500,
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: 0.86,
-    letterSpacing: "normal",
-    textAlign: "center",
-    color: "black",
-  },
-
-  middleBar: {
-    //middleBar는 전체의 10%만큼,  그라데이션의 25%차지
-    height: "25%",
-    width: "100%",
-
-    //
-    backgroundColor: "white",
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-
-    //
-    display: "flex",
-    justifyContent: "center",
-  },
-  middleBarTextContainer: {
-    //middleBar(뷰포트 가로 전체)의 80%
-    width: "80%",
-    //middleBar의 높이 100%(높이도 설정해주지 않으면, content 크기만큼만 가져감)
-    height: "100%",
-    //
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  middleBarText: {
-    textDecoration: "none",
-    color: "#627cec",
-    fontWeight: "bold",
-    fontSize: "1.5em",
-  },
-
-  information: {
-    //information은 전체의 60%만큼 차지
-    height: "60%",
-    width: "100%",
-
-    //! 왜 안먹히니..
-    borderTopWidth: "10px",
-    borderTopColor: "black",
-  },
-
-  mainFirst: {
-    //이름과 호칭을 가로로 배열하기 위해
-    display: "flex",
-  },
 
   //todo Link 태그의 text에 인라인 방식으로 적용한 스타일들 styled component로 변경해야함.
 };
